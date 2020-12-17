c6a8af0ba32a4fc35db734fd94dfebf9e741bb6c
James Carroll <jamcar23@gmail.com>
2020-12-17 12:28:39 -0600

    refactor(numpy): move calc_interp function back into get_interp

--- common/numpy_fast.py
+++ common/numpy_fast.py
@@ -9,16 +9,13 @@ def interp(x, xp, fp):
   T = len(fp)
 
   def get_interp(xv):
-    def calc_interp(l, h):
-      return (xv - xp[l]) * (fp[h] - fp[l]) / (xp[h] - xp[l]) + fp[l]
-
     hi = 0
     while hi < N and xv > xp[hi]:
       hi += 1
     low = hi - 1
     return fp[-1] if hi >= T or low >= T or (hi >= N and xv >= xp[low]) or (N < T and xv == xp[-1]) else (
       fp[0] if hi == 0 else
-      calc_interp(low, hi))
+      (xv - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]) + fp[low])
 
   return [get_interp(v) for v in x] if hasattr(x, '__iter__') else get_interp(x)
 
