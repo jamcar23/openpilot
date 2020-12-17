20799b245be51432e2136417d49f2f580a2b6c8d
James Carroll <jamcar23@gmail.com>
2020-12-17 12:10:25 -0600

    tests(numpy): add more test cases for when v is shorter

--- common/tests/test_numpy_fast.py
+++ common/tests/test_numpy_fast.py
@@ -42,19 +42,21 @@ class InterpTest(unittest.TestCase):
   
   def test_long_bp_short_v(self):
     _A_CRUISE_MIN_BP = np.asarray([0., 5., 10., 20., 40.])
-    _A_CRUISE_MIN_V = np.asarray([-1.0, -.8, -.67])
-    _A_CRUISE_MIN_V_EXPECTED = np.asarray([-1.0, -.8, -.67, -.67, -.67])
+    v_test_cases = ([-1.0], [-1.0, -.8], [-1.0, -.8, -.67], [-1.0, -.8, -.67, -.5])
+    v_expected_cases = ([-1.0, -1., -1., -1., -1.], [-1.0, -.8, -.8, -.8, -.8], [-1.0, -.8, -.67, -.67, -.67], [-1.0, -.8, -.67, -.5, -.5])
     v_ego_arr = [-1, -1e-12, 0, 4, 5, 6, 7, 10, 11, 15.2, 20, 21, 39,
                  39.999999, 40, 41]
 
-    expected = np.interp(v_ego_arr, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V_EXPECTED)
-    actual = interp(v_ego_arr, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V)
-    np.testing.assert_equal(actual, expected)
+    for v_a, v_e in zip(v_test_cases, v_expected_cases):
+      with self.subTest(msg='Checking if v_a equals v_e', v_a=v_a, v_e=v_e):
+        expected = np.interp(v_ego_arr, _A_CRUISE_MIN_BP, v_e)
+        actual = interp(v_ego_arr, _A_CRUISE_MIN_BP, v_a)
+        np.testing.assert_equal(actual, expected)
 
-    for v_ego in v_ego_arr:
-      expected = np.interp(v_ego, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V_EXPECTED)
-      actual = interp(v_ego, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V)
-      np.testing.assert_equal(actual, expected)
+        for v_ego in v_ego_arr:
+          expected = np.interp(v_ego, _A_CRUISE_MIN_BP, v_e)
+          actual = interp(v_ego, _A_CRUISE_MIN_BP, v_a)
+          np.testing.assert_equal(actual, expected)
 
 
 if __name__ == "__main__":
