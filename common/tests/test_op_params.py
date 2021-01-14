9d947c513f0f716c2ed256f148d43bdcfce31958
James Carroll <jamcar23@gmail.com>
2021-01-13 23:07:40 -0600

    debug(params): print multi-bp array in tests

--- common/tests/test_op_params.py
+++ common/tests/test_op_params.py
@@ -10,16 +10,13 @@ class OpParamsTest(unittest.TestCase):
     op_params = opParams()
     bps = [[0, 10], [[20, 24], [20, 24, 30]]]
     v = [[5, 5.75], [6, 7.25, 7.5]]
-    v_ego_arr = [-1, -1e-12, 0, 4, 5, 6, 7, 10, 11, 15.2, 20, 21, 39,
-                 39.999999, 40, 41]
-    # Todo: write function to interp bp args
+    steer_vego_arr = [[-1, -1e-12, 0, 4, 5, 6, 7, 10, 11], [-1, -1e-12, 0, 4, 5, 6, 7, 10, 11, 15.2, 20, 21, 39,
+                 39.999999, 40, 41]]
+    # Todo: write function to interp bp args    
+    actual = interp(steer_vego_arr, bps, v)
+    print(f'actual: {actual}')
 
-    expected = np.interp(v_ego_arr, bps, v)
-    actual = interp(v_ego_arr, bps, v)
-    
-    np.testing.assert_equal(actual, expected)
-
-    for v_ego in v_ego_arr:
-      expected = np.interp(v_ego, bps, v)
+    for v_ego in steer_vego_arr:
       actual = interp(v_ego, bps, v)
-      np.testing.assert_equal(actual, expected)
+      print(f'actual: {actual}'
+      
