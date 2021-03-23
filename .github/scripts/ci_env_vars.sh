705fb2b23c64be8317737299e6f6b1d13822e851
James Carroll <jamcar23@gmail.com>
2021-03-23 15:24:16 -0500

    fix(github): fix bash issues in ci_env_vars.sh

--- .github/scripts/ci_env_vars.sh
+++ .github/scripts/ci_env_vars.sh
@@ -2,6 +2,10 @@
 
 # Script that sets various environmental variables for the rest of the CI pipeline.
 
+# Local vars
+__eval_true='true'
+__eval_false='' # Empty string because non-empty string are always true (at least for github actions / workflows)
+
 # Common vars used to make bigger, exported env vars.
 REPO="jamcar23/openpilot"
 REGISTRY_BASE="ghcr.io"
@@ -11,7 +15,7 @@ UNIT_TEST="coverage run --append -m unittest discover"
 
 CONTAINER_URI="$REGISTRY_BASE/$REPO/openpilot"
 BASE_IMAGE="$CONTAINER_URI-base:latest"
-CI_IMAGE="$(CONTAINER_URI)ci:latest"
+CI_IMAGE="${CONTAINER_URI}ci:latest"
 FAT_IMAGE="$CONTAINER_URI-fat:latest"
 
 RUN_CMD_BASE="docker run --shm-size 1G -e PYTHONPATH=/tmp/openpilot -e GITHUB_ACTION -e GITHUB_REF -e GITHUB_HEAD_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_RUN_ID -v /tmp/comma_download_cache:/tmp/comma_download_cache"
@@ -22,9 +26,9 @@ function eval_var()
     local __result_var=$1
 
     if [[ $2 = $3 ]]; then
-        local __result='true'
+        local __result=$__eval_true
     else
-        local __result='' # Empty string because non-empty string are always true
+        local __result=$__eval_false
     fi
 
     eval $__result_var="$__result"
@@ -34,19 +38,21 @@ function eval_var()
 eval_var IS_DEFAULT $GITHUB_REPOSITORY $REPO
 eval_var IS_MAIN $GITHUB_REF "refs/heads/$MAIN_BRANCH"
 
-if [[ $GITHUB_EVENT_NAME = 'push' ] && [ $IS_DEFAULT ] && [ $IS_MAIN ]]; then
+if [ [ $GITHUB_EVENT_NAME = 'push' ] && [ $IS_DEFAULT = $__eval_true ] && [ $IS_MAIN = $__eval_true ] ]; then
     MAIN_IMAGE="$FAT_IMAGE"
     BUILD_CMD="docker build -t $BASE_IMAGE -f Dockerfile.openpilot_base . && docker build -t $CI_IMAGE -f Dockerfile.openpilotci --cached-from $BASE_IMAGE . && docker build -t $FAT_IMAGE -f Dockerfile.openpilot_fat --cached-from $CI_IMAGE $BASE_IMAGE ."
     RUN_CMD="$RUN_CMD_BASE --name $TEMP_IMAGE"
     SHOULD_BUILD_DOCKER='true'
+    echo 'true'
 else
     MAIN_IMAGE="$CI_IMAGE"
     BUILD_CMD="docker pull $CI_IMAGE"
     RUN_CMD="$RUN_CMD_BASE -v $PWD:/tmp/openpilot --rm"
     SHOULD_BUILD_DOCKER=''
+    echo 'false'
 fi
 
-RUN_CMD = "$RUN_CMD $MAIN_IMAGE /bin/sh -c"
+RUN_CMD="$RUN_CMD $MAIN_IMAGE /bin/sh -c"
 
 # Export Environment
 echo "IS_DEFAULT=$IS_DEFAULT" >> $GITHUB_ENV
