1e917a7b05cf0377663ebaee1e974e2e8c49e1e7
James Carroll <jamcar23@gmail.com>
2021-03-23 14:24:47 -0500

    fix(github): properly eval conditions and export results in bash script

--- .github/scripts/ci_env_vars.sh
+++ .github/scripts/ci_env_vars.sh
@@ -2,14 +2,12 @@
 
 # Script that sets various environmental variables for the rest of the CI pipeline.
 
-# Common "global" vars available to all and are used to build bigger variables.
-
-export REPO="jamcar23/openpilot"
-export REGISTRY_BASE="ghcr.io"
-export MAIN_BRANCH="src"
-export TEMP_IMAGE="tmppilotci"
-export UNIT_TEST="coverage run --append -m unittest discover"
-
+# Common vars used to make bigger, exported, env vars.
+REPO="jamcar23/openpilot"
+REGISTRY_BASE="ghcr.io"
+MAIN_BRANCH="src"
+TEMP_IMAGE="tmppilotci"
+UNIT_TEST="coverage run --append -m unittest discover"
 RUN_CMD_BASE="docker run --shm-size 1G -e PYTHONPATH=/tmp/openpilot -e GITHUB_ACTION -e GITHUB_REF -e GITHUB_HEAD_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_RUN_ID -v /tmp/comma_download_cache:/tmp/comma_download_cache"
 
 # Functions
@@ -27,10 +25,7 @@ function eval_var()
 }
 
 # Evaluate Environment
-
-eval_var IS_DEFAULT $GITHUB_REPOSITORY = $REPO
-
-echo "Is default: $IS_DEFAULT"
+eval_var IS_DEFAULT $GITHUB_REPOSITORY $REPO
 
 # Export Environment
-export IS_DEFAULT
+echo "IS_DEFAULT=$IS_DEFAULT" >> $GITHUB_ENV
