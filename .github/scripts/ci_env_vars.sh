f77f67f131d6cc86ab43002a841e1c0e600086a7
James Carroll <jamcar23@gmail.com>
2021-03-23 15:08:06 -0500

    feat(github): export all env vars from bash script to setup_workflow job

--- .github/scripts/ci_env_vars.sh
+++ .github/scripts/ci_env_vars.sh
@@ -2,12 +2,18 @@
 
 # Script that sets various environmental variables for the rest of the CI pipeline.
 
-# Common vars used to make bigger, exported, env vars.
+# Common vars used to make bigger, exported env vars.
 REPO="jamcar23/openpilot"
 REGISTRY_BASE="ghcr.io"
 MAIN_BRANCH="src"
 TEMP_IMAGE="tmppilotci"
 UNIT_TEST="coverage run --append -m unittest discover"
+
+CONTAINER_URI="$REGISTRY_BASE/$REPO/openpilot"
+BASE_IMAGE="$CONTAINER_URI-base:latest"
+CI_IMAGE="$(CONTAINER_URI)ci:latest"
+FAT_IMAGE="$CONTAINER_URI-fat:latest"
+
 RUN_CMD_BASE="docker run --shm-size 1G -e PYTHONPATH=/tmp/openpilot -e GITHUB_ACTION -e GITHUB_REF -e GITHUB_HEAD_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_RUN_ID -v /tmp/comma_download_cache:/tmp/comma_download_cache"
 
 # Functions
@@ -26,6 +32,30 @@ function eval_var()
 
 # Evaluate Environment
 eval_var IS_DEFAULT $GITHUB_REPOSITORY $REPO
+eval_var IS_MAIN $GITHUB_REF "refs/heads/$MAIN_BRANCH"
+
+if [[ $GITHUB_EVENT_NAME = 'push' ] && [ $IS_DEFAULT ] && [ $IS_MAIN ]]; then
+    MAIN_IMAGE="$FAT_IMAGE"
+    BUILD_CMD="docker build -t $BASE_IMAGE -f Dockerfile.openpilot_base . && docker build -t $CI_IMAGE -f Dockerfile.openpilotci --cached-from $BASE_IMAGE . && docker build -t $FAT_IMAGE -f Dockerfile.openpilot_fat --cached-from $CI_IMAGE $BASE_IMAGE ."
+    RUN_CMD="$RUN_CMD_BASE --name $TEMP_IMAGE"
+    SHOULD_BUILD_DOCKER='true'
+else
+    MAIN_IMAGE="$CI_IMAGE"
+    BUILD_CMD="docker pull $CI_IMAGE"
+    RUN_CMD="$RUN_CMD_BASE -v $PWD:/tmp/openpilot --rm"
+    SHOULD_BUILD_DOCKER=''
+fi
+
+RUN_CMD = "$RUN_CMD $MAIN_IMAGE /bin/sh -c"
 
 # Export Environment
 echo "IS_DEFAULT=$IS_DEFAULT" >> $GITHUB_ENV
+echo "IS_MAIN=$IS_MAIN" >> $GITHUB_ENV
+echo "URI=$CONTAINER_URI" >> $GITHUB_ENV
+echo "BASE_IMAGE=$BASE_IMAGE" >> $GITHUB_ENV
+echo "CI_IMAGE=$CI_IMAGE" >> $GITHUB_ENV
+echo "FAT_IMAGE=$FAT_IMAGE" >> $GITHUB_ENV
+echo "MAIN_IMAGE=$MAIN_IMAGE" >> $GITHUB_ENV
+echo "BUILD_CMD=$BUILD_CMD" >> $GITHUB_ENV
+echo "RUN_CMD=$RUN_CMD" >> $GITHUB_ENV
+echo "SHOULD_BUILD_DOCKER=$SHOULD_BUILD_DOCKER" >> $GITHUB_ENV
