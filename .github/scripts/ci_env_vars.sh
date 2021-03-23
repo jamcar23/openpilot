e9ae73cb98915ed26096d450e6bbde4459d813ac
James Carroll <jamcar23@gmail.com>
2021-03-23 15:31:15 -0500

    fix(github): fix syntax error in bash ci env var script

--- .github/scripts/ci_env_vars.sh
+++ .github/scripts/ci_env_vars.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
 # Script that sets various environmental variables for the rest of the CI pipeline.
-
-# Local vars
-__eval_true='true'
-__eval_false='' # Empty string because non-empty string are always true (at least for github actions / workflows)
-
 # Common vars used to make bigger, exported env vars.
+# EDIT THESE VARS HERE if you're a fork maintainer.
 REPO="jamcar23/openpilot"
 REGISTRY_BASE="ghcr.io"
 MAIN_BRANCH="src"
@@ -19,6 +15,10 @@ CI_IMAGE="${CONTAINER_URI}ci:latest"
 FAT_IMAGE="$CONTAINER_URI-fat:latest"
 
 RUN_CMD_BASE="docker run --shm-size 1G -e PYTHONPATH=/tmp/openpilot -e GITHUB_ACTION -e GITHUB_REF -e GITHUB_HEAD_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_RUN_ID -v /tmp/comma_download_cache:/tmp/comma_download_cache"
+
+# Local vars, generally don't change these.
+__eval_true='true'
+__eval_false='' # Empty string because non-empty string are always true (at least for github actions / workflows)
 
 # Functions
 function eval_var()
@@ -38,7 +38,7 @@ function eval_var()
 eval_var IS_DEFAULT $GITHUB_REPOSITORY $REPO
 eval_var IS_MAIN $GITHUB_REF "refs/heads/$MAIN_BRANCH"
 
-if [ [ $GITHUB_EVENT_NAME = 'push' ] && [ $IS_DEFAULT = $__eval_true ] && [ $IS_MAIN = $__eval_true ] ]; then
+if [[ $GITHUB_EVENT_NAME = 'push' && $IS_DEFAULT = $__eval_true && $IS_MAIN = $__eval_true ]]; then
     MAIN_IMAGE="$FAT_IMAGE"
     BUILD_CMD="docker build -t $BASE_IMAGE -f Dockerfile.openpilot_base . && docker build -t $CI_IMAGE -f Dockerfile.openpilotci --cached-from $BASE_IMAGE . && docker build -t $FAT_IMAGE -f Dockerfile.openpilot_fat --cached-from $CI_IMAGE $BASE_IMAGE ."
     RUN_CMD="$RUN_CMD_BASE --name $TEMP_IMAGE"
