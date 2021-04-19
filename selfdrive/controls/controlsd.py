f83b308338aa30c6877378e17427ccc9e73d86dd
James Carroll <jamcar23@gmail.com>
2021-04-19 17:59:07 -0500

    fix(controls): make sure the speed limit order of operations is correct

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -294,12 +294,11 @@ class Controls:
 
     self.v_cruise_kph_last = self.v_cruise_kph
 
-    if self.opParams.get(ENABLE_ROAD_SIGNS) and CS.speedLimitValid and CS.speedLimitKph > 0:
-      self.v_cruise_kph = CS.speedLimitKph
-
     # if stock cruise is completely disabled, then we can use our own set speed logic
     if not self.CP.enableCruise:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.enabled)
+    elif self.opParams.get(ENABLE_ROAD_SIGNS) and CS.speedLimitValid and CS.speedLimitKph > 0:
+      self.v_cruise_kph = CS.speedLimitKph
     elif self.CP.enableCruise and CS.cruiseState.enabled:
       self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
 
