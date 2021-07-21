fc7fcd9e47a9948e02e83400e79796b47ab7dbea
James Carroll <jamcar23@gmail.com>
2021-07-20 19:26:42 -0500

    hotfix(tools): don't change op version when generating changelog

--- tools/scripts/gen_changelog.py
+++ tools/scripts/gen_changelog.py
@@ -196,12 +196,12 @@ def get_next_semantic_version_number(last_entry, op_version):
 
   return increment_semantic_version(last_entry.fp_version, semver_change) if last_entry.fp_version else '1.0.0'
 
-def export_next_semver_number():
+def export_semver_number(next):
   last_entry = get_last_entry_from_log()
   last_commit = get_git_log(['-1', '--pretty=format:"%h"']).replace('"', '').strip()
   op_version = get_commit_op_version(last_commit)
 
-  print(get_next_semantic_version_number(last_entry, op_version))
+  print(get_next_semantic_version_number(last_entry, op_version) if next else last_entry.fp_version)
 
 class PullRequest:
   def __init__(self, commit_hash, base_hash, head_hash):
@@ -339,8 +339,9 @@ def main():
 
 if __name__ == '__main__':
   args = sys.argv[1:]
+  export_nxt = '--export-next' in args
 
-  if '--export' in args:
-    export_next_semver_number()
+  if '--export' in args or export_nxt:
+    export_semver_number(export_nxt)
   else:
     main()
